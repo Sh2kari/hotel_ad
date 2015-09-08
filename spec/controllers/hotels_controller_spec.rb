04ad13356026d026e21f08cf4ca9f3645635f46f
Sh2kari <buba_sev@bk.ru>
2015-09-08 12:56:56 +0300

    spec

--- spec/controllers/hotels_controller_spec.rb
+++ spec/controllers/hotels_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe HotelsController, type: :controller do
+  def valid_attributes
+    { title: "title", room_description: "rock", 
+      price: "323", breakfast: "no" }
+  end
+
+  describe "GET index" do
+    it "assigns all @hotels" do
+    hotel = Hotel.create! valid_attributes
+    get :index
+    expect(assigns(:hotels)).to eq([hotel])
+    end
+  end
 end
